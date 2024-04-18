c57670fc52518f960ed6c9a71e46755fbf73b057
Codru <135108574+Codru1@users.noreply.github.com>
2024-04-18 21:23:15 +0300

    Create tema2.cpp

---
+++ schoolWork/TIC/tema2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath> 
+
+using namespace std;
+
+int main() {
+    float L, D;
+
+    cout << "Introduceti lungimea laturii patratului: ";
+    cin >> L;
+
+   
+    D = L * sqrt(2);
+
+   
+    cout << "Diagonala pătratului este: " << D ;
+
+
+}
